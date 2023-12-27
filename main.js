1dab375422d8203daa60e7f4d938102f2aa69ef4
Leslie Raganit <lnraganit@gmail.com>
2023-12-27 11:44:14 -0600

    questions from fall in love questionaire added

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const questions = require("./questions.json");
 const amountOfQuestions = questions.length;
 
-function randomquestion() {
+function randomQuestion() {
   const randomValue = Math.floor(Math.random() * amountOfQuestions);
   return questions[randomValue];
 }
 
-console.log(randomquestion());
+const visibleQuestion = document.getElementsByClassName("visible-question");
+visibleQuestion.innterHTML = randomQuestion();
+console.log(visibleQuestions);

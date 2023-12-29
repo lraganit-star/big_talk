38c2d913ef3c29b87b44974be123522a2164c8e3
Leslie Raganit <lnraganit@gmail.com>
2023-12-29 00:35:09 -0600

    friendship questionairre added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const bigTalkJSON = require("../questions/big_talk.json");
+const fallInLoveJSON = require("../questions/fall_in_love.json");
 
 app.use(express.json());
 app.use(cors());
@@ -13,11 +14,12 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/big_talk", (req, res) => {
   res.json(bigTalkJSON);
-  console.log("Data read");
+  console.log("big talk data read");
 });
 
-app.post("/big_talk", (req, res) => {
-  res.send("Data received");
+app.get("/fall_in_love", (req, res) => {
+  res.json(fallInLoveJSON);
+  console.log("fall in love data read");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

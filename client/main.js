333eb6b5118b62d847a10467c40092120b6779fe
Leslie Raganit <lnraganit@gmail.com>
2023-12-28 23:43:06 -0600

    question changes when button is clicked

--- client/main.js
+++ client/main.js
@@ -1,20 +1,12 @@
-let questionNumber = 0;
-
-// // figure out how to get increase the question number when button is clicked
-// document.getElementsByClassName("question-number")[0].onclick = function () {
-//   questionNumber++;
-// };
-
 fetch("/big_talk")
   .then((response) => response.json())
   .then((data) => {
-    const visibleQuestions =
-      document.getElementsByClassName("visible-question");
-    if (visibleQuestions.length > 0) {
+    let questionNumber = 0;
+    addEventListener("click", () => {
+      const visibleQuestions =
+        document.getElementsByClassName("visible-question");
+      questionNumber++;
       visibleQuestions[0].innerHTML = data[questionNumber];
-      console.log(data[questionNumber]);
-    } else {
-      console.log('No elements with class "visible-question" found.');
-    }
+    });
   })
   .catch((error) => console.error("Error:", error));

208c19d120e060c8578b58285eca87ca463b7e8c
Leslie Raganit <lnraganit@gmail.com>
2023-12-28 23:57:28 -0600

    added progress bar

--- client/main.js
+++ client/main.js
@@ -7,6 +7,9 @@ fetch("/big_talk")
         document.getElementsByClassName("visible-question");
       questionNumber++;
       visibleQuestions[0].innerHTML = data[questionNumber];
+
+      let v1 = document.getElementById("p1").value;
+      document.getElementById("p1").value = v1 + 1;
     });
   })
   .catch((error) => console.error("Error:", error));

e4753b4b5c980e47e93111d0e6640f0541cd350b
Leslie Raganit <lnraganit@gmail.com>
2023-12-28 20:05:03 -0600

    server and client connected

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 fetch("/data")
   .then((response) => response.json())
   .then((data) => {
-    console.log("hello world");
+    console.log(data);
   })
   .catch((error) => console.error("Error:", error));
 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 const myData = require("../questions/big_talk.json");
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+app.use(express.static("../client"));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/data", (req, res) => {
   res.json(myData);
+  console.log("Data read");
+});
+
+app.post("/data", (req, res) => {
+  res.send("Data received");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.use(express.static("../client"));
